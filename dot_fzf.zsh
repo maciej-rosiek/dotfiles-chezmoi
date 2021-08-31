35335a50c9633cd11bdab77c9f5f250dd4299f0a
Maciej Rosiek <maciej.rosiek@gmail.com>
2021-08-31 17:49:49 +0200

    Initial\

---
+++ dot_fzf.zsh
@@ -0,0 +1,14 @@
+# Setup fzf
+# ---------
+# if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+#   export PATH="$PATH:/usr/local/opt/fzf/bin"
+# fi
+
+# Auto-completion
+# ---------------
+# [[ $- == *i* ]] && source "/usr/share/fzf/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+# source "/usr/share/fzf/key-bindings.zsh"
+
