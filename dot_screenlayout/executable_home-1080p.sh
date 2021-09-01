dbca435bf294020a182ab97eef7786c12ac9f597
Maciej Rosiek <maciej.rosiek@gmail.com>
2021-09-01 09:49:22 +0200

    Initial

---
+++ dot_screenlayout/executable_home-1080p.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP --mode 1920x1080 --right-of HDMI-A-0 --pos 3440x542 --rotate normal --brightness .5 --output DisplayPort-0 --off --output HDMI-A-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal
